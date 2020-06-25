d8559a8467d6b435224a47e7b937a9e6c685f3fb
Bobby Neal <rsneal01@gmail.com>
2020-06-25 16:53:12 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
   
+  module ClassMethods
+    
   def reset_all
-    all.clear
+    self.all.clear
   end
 
   def count
-    all.count
+    self.all.count
   end
   
+  module InstanceMethods
+    
+    def initialize
+      self.class.all << self
 end
